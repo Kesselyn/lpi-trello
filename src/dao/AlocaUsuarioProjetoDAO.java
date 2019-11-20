2fdfc1ac26af16f246803fe350d6512e1bee55a2
kesselyn <kesselynfortunato@hotmail.com>
2019-11-20 19:13:29 -0300

    Primeira consulta funcionando

--- src/dao/AlocaUsuarioProjetoDAO.java
+++ src/dao/AlocaUsuarioProjetoDAO.java
@@ -2,6 +2,9 @@ package dao;
 
 import java.sql.*;
 import model.AlocaUsuarioProjeto;
+import model.Projeto;
+import model.Usuario;
+import java.util.ArrayList;
 
 public class AlocaUsuarioProjetoDAO {
     private Connection conexao;
@@ -55,5 +58,65 @@ public class AlocaUsuarioProjetoDAO {
 			System.err.println("Falha delete mensagem no java: " + e.getMessage());
 			e.printStackTrace();
 		}
-	}
+    }
+    
+    //A classe é do tipo array de projetos pois é o tipo que quero retornar
+    //Ela recebe AlocaProjetoUsuario como parâmetro pq é onde estão os dados que vou utilizar na consulta
+    public ArrayList <Projeto> readProjetosUsuario(AlocaUsuarioProjeto alocaUsuarioProjeto) {
+		
+        String consulta = "SELECT id_projeto, nome_projeto, lista_coluna, usuario_proprietario "
+                            + " FROM aloca_usuario_projeto AS A"
+                            + " INNER JOIN Projeto AS P"
+                            + " ON A.fk_projeto = P.id_projeto"
+                            + " AND fk_usuario = ?";
+        
+        try (PreparedStatement pst = conexao.prepareStatement(consulta)) {
+            
+            pst.setString(1, alocaUsuarioProjeto.getUsuario().getApelido());
+            
+            //executando a busca
+            ResultSet resultado = pst.executeQuery();
+            
+            //Array que vai guardar os dados que eu quero buscar
+            //como quero apenas dados do projeto o array será do tipo projeto
+            ArrayList<Projeto> projetos = new ArrayList();
+            
+            Projeto projeto = null;
+            Usuario usuario = null;
+            
+            //Percorrendo todas as linhas retornadas do banco de dados
+            while (resultado.next()) {
+                projeto = new Projeto();
+                usuario = new Usuario();
+
+                // pegando os valores da linha da vez:
+                int idProjeto = resultado.getInt("id_projeto");
+                String nomeProjeto = resultado.getString("nome_projeto");
+                String colunaProjeto = resultado.getString("lista_coluna");
+                String usuarioProprietario = resultado.getString("usuario_proprietario");
+                
+                //inserindo os dados nos objetos para serem colocados no array depois
+                usuario.setApelido(usuarioProprietario);
+                
+                projeto.setIdentificadorProjeto(idProjeto);
+                projeto.setNomeProjeto(nomeProjeto);
+                projeto.setLista(colunaProjeto);
+                projeto.setUsuarioProprietario(usuario);
+
+                //Adicionando o objeto da vez ArrayList
+                projetos.add(projeto);
+            }
+            
+            //retorno do array com os dados
+            return projetos;
+            
+        }
+        catch (SQLException ex) {
+            // Se acontecer alguma exceção imprima a pilha de erros
+            ex.printStackTrace();
+        }
+        
+        // se acontecer algum problema
+        return null;
+    }
 }
